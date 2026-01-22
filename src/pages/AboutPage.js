d3f3c00d2b99886be590ee8470af165c4d1586e3
Raja <raja.hussain300@gmail.com>
2026-01-21 19:01:34 -0500

    Fix unused imports

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import About from "../components/about/About";
-import Footer from "../components/footer/Footer";
 
 function AboutPage() {
   return (
--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Projects from "../components/projects/Projects";
-import exampleImage from "../assets/construction.png";
 function ProjectsPage() {
   return <Projects />;
 }
